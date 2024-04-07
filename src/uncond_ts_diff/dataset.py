f908745aa787bd9c68a6371aca49df85ebd48215
Nam T. Tran <90613489+TTNamUS@users.noreply.github.com>
2024-04-07 17:05:55 +0700

    add get_crypto dataset

--- src/uncond_ts_diff/dataset.py
+++ src/uncond_ts_diff/dataset.py
@@ -4,9 +4,16 @@ import os
 import tarfile
 from pathlib import Path
 from urllib import request
-
+import datetime
+import requests
+import bs4
+import yfinance as yf
+from gluonts.dataset.common import ListDataset,MetaData,TrainDatasets
 from gluonts.dataset.common import load_datasets
 from gluonts.dataset.repository.datasets import get_dataset, get_download_path
+from gluonts.dataset.split import split
+import numpy as np
+
 
 default_dataset_path: Path = get_download_path() / "datasets"
 wiki2k_download_link: str = "https://github.com/awslabs/gluonts/raw/b89f203595183340651411a41eeb0ee60570a4d9/datasets/wiki2000_nips.tar.gz"  # noqa: E501
@@ -34,3 +41,68 @@ def get_gts_dataset(dataset_name):
         )
     else:
         return get_dataset(dataset_name)
+
+def get_crypto( prediction_length: int,freq: str="H",split_offset: int = None):
+    start_date = '2023-01-01'
+    #change format date
+    start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    print(start_date)
+    list_crypto = []
+    url='https://coinmarketcap.com/coins/'
+    res = requests.get(url)
+    soup = bs4.BeautifulSoup(res.content, 'html.parser')
+    # get data from class "sc-4984dd93-0 iqdbQL coin-item-symbol"
+    data = soup.find_all("p", class_="sc-4984dd93-0 iqdbQL coin-item-symbol")
+    for i in data:
+        if 'USD' not in i.text:
+            print(i.text)
+            list_crypto.append(i.text)
+
+    list_data=[]
+    for crypto in list_crypto:    
+        data = yf.download(crypto+'-USD', interval='1h', start=start_date,end="2024-01-01")
+        data.reset_index()
+        data = data.rename(columns={ 'Close':'target'})
+        list_data.append(data['target'].values.tolist())
+
+
+    list_data=list_data[:5]
+    training_data=[]
+    test_data=[]    
+    for price in list_data:
+            #split data to train and test
+            split = int(len(price)-24)
+            train = price[:split]
+            test = price
+            print(len(train),len(test))
+            i=list_data.index(price)
+            training_data.append({"start": start_date, "target": train,"feat_static_cat":np.array([i])})
+            test_data.append({"start": start_date, "target": test,"feat_static_cat":np.array([i])})
+            
+
+
+    dataset_train = ListDataset(
+        data_iter=training_data,
+        freq=freq
+    )    
+    dataset_test = ListDataset(
+        data_iter=test_data,
+        freq=freq
+    )
+    #merge
+    dataset = TrainDatasets(
+        metadata=MetaData(
+            freq=freq,
+            prediction_length=prediction_length,
+            start=start_date,
+        ),
+        train=dataset_train,
+        test=dataset_test,
+    )
+
+    return dataset
+
+if __name__ == "__main__":
+    data= get_crypto(prediction_length=24)
+    print(data)
+    
